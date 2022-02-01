c83a5e037fb4be72501a0ac0a3cf090f93ef02f2
GitSion <74344298+GitSion@users.noreply.github.com>
2022-02-01 23:57:50 +0900

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,21 +1,22 @@
 from tkinter import *
 from tkinter.messagebox import *
 from time import sleep
-root = Tk()
+import os
 
-email_string = 'sion011218@naver.com,qwfasdf34@naver.com,23q2erfqwd@daum.net,asfgqwerg2342@gmail.com'
+root = Tk()
+root.resizable(False,False)
+email_string = 'sion011218@naver.com,qwfasdf34@naver.com,23q2erfqwd@daum.net,asfgqwerg2342@gmail.com,wqff32@hanmail.net,423dfw@hkd.hs'
 
 email_lists = email_string.split(',')
 
-
 # 컴포넌트 정의
 root.title('댓글 email 가져오기')
-root.geometry('340x500+200+200')
+#root.geometry('400x500')
 
-blog_address_label = Label(root, text='네이버 블로그 주소\n')
+blog_address_label = Label(root, text='네이버 블로그 주소')
 blog_address_entry = Entry(root)
 
-id_label = Label(root, text='ID\n')
+id_label = Label(root, text='ID')
 id_entry = Entry(root)
 
 pwd_label = Label(root, text='PWD')
@@ -23,11 +24,53 @@ pwd_entry = Entry(root)
 
 ok_button_1 = Button(root, text='확인')
 
+null_label1 = Label(root,text='''
+ ''')
+def ok_button_1_click(string):
+
+    global crawling_cmt_label
+    crawling_cmt_label = Label(root, text='해당 포스트의 댓글에 달린 이메일들을 가져왔습니다!')
+    crawling_cmt_label.grid(row=4, column=0, columnspan=3, sticky='ew')
+
+    blog_address = blog_address_entry.get()
+    ids = id_entry.get()
+    pwd = pwd_entry.get()
+
+    print(blog_address, ids, pwd)
+
+ok_button_1.bind('<Button-1>', ok_button_1_click)
+
+null_label2 = Label(root,text='''
+''')
+
 from_email_label = Label(root, text='어떤 email부터 가져올까요?')
 from_email_entry = Entry(root)
-ok_button_2 = Button(root, text='확인')
+ok_button_2 = Button(root,text='확인')
 
-unvalid_email_label = Label(root, text='의심되는 이메일들')
+def ok_button_2_click(string):
+    std_email = from_email_entry.get()
+    # std_email = asfgqwerg2342@gmail.com
+    idx = email_lists.index(std_email)
+    result_list = email_lists[:idx+1]
+    print(result_list)
+    print_clear_label = Label(root,text='이메일을 모두 가져왔습니다!')
+    print_clear_label.grid(row=7, column=0, sticky='ew')
+    with open('result.txt','w') as f:
+        for i in result_list:
+            f.write(i)
+
+    view_file_button = Button(root,text='결과 파일 보기')
+    view_file_button.grid(row=7, column=1, columnspan=2, sticky='ew')
+
+    def view_file_button_click(string):
+        os.system('C:\\Users\\CKIRUser\\PycharmProjects\\pythonProject\\result.txt')
+
+    view_file_button.bind('<Button-1>', view_file_button_click)
+
+ok_button_2.bind('<Button-1>', ok_button_2_click)
+
+
+
 
 # 배치
 blog_address_label.grid(row=0, column=0, sticky='ew')
@@ -41,54 +84,17 @@ pwd_entry.grid(row=2, column=1, sticky='ew')
 
 ok_button_1.grid(row=0, column=3, rowspan=3, sticky='sn')
 
+null_label1.grid(row=3, column=0, columnspan=3, sticky='ew')
 
-def ok_button_1_click(string):
-    crawling_cmt_label = Label(root, text='해당 포스트의 댓글에 달린 이메일들을 가져왔습니다!')
-    crawling_cmt_label.grid(row=4, column=0, columnspan=3, sticky='ew')
+null_label2.grid(row=5, column=0, columnspan=3, sticky='ew')
 
-    blog_address = blog_address_entry.get()
-    ids = id_entry.get()
-    pwd = pwd_entry.get()
+from_email_label.grid(row=6, column=0, sticky='ew')
+from_email_entry.grid(row=6, column=1, sticky='ew')
 
-    print(blog_address, ids, pwd)
-
-
-ok_button_1.bind('<Button-1>', ok_button_1_click)
-
-null_label = Label(root, text='''
-
-
-''')
-null_label.grid(row=4, column=0, columnspan=3, sticky='ew')
-
-from_email_label.grid(row=5, column=0, sticky='ew')
-from_email_entry.grid(row=5, column=1, sticky='ew')
-ok_button_2.grid(row=6, column=0, columnspan=4, sticky='ew')
-
-
-def ok_button_2_click(string):
-    # 이메일들을 정상적으로 가져왔을 때만 실행, 아니면 except
-    #ending_email = from_email_entry.get()
-    #print(ending_email)
-    loading_label = Label(root, text='로딩 중...')
-    loading_label.grid(row=7, column=0, columnspan=3, sticky='ew')
-
-ok_button_2.bind('<Button-1>', ok_button_2_click)
-
-sleep(3)
+ok_button_2.grid(row=6, column=2, sticky='ew')
 
 
 
-if True:
-    loading_label.config(text='성공적으로 불러왔습니다! 이메일 리스트가 적힌 메모장 파일을 저장하세요.')
-    
-    see_result_button = Button(root,text='이메일 리스트 보기')
-    see_result_button.grid()
-    def see_result_button_click(string):
-
-
-result_label = Label(root,text=str(email_lists))
-result_label.grid(row=8, column=0, columnspan=3, sticky='ew')
 
 root.mainloop()
 
