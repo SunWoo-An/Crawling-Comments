20062f0f80404ee0d6419c5bff4874b69f065ea9
SunWoo-An <84854953+SunWoo-An@users.noreply.github.com>
2022-02-01 23:31:54 +0900

    Update Inner.py
    + driver 가 제대로 작동하지 않는 부분을 고침. 
    + 댓글창을 왼쪽으로 넘기는 부분에서 driver.implicitly_wait(5) 를 해주지 않으면 오류가 생기는 부분이 있음. 이에 대해선 자세히 알아볼 필요가 있었음.
    
    현재 문제점 ) 메모장에 제대로 글이 안써지는 점.

--- Inner.py
+++ Inner.py
@@ -4,44 +4,65 @@ from time import sleep
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+
+import Functions
 from Functions import *
 
-   
 if __name__ == '__main__':
-    Check_Last_Mail = False
 
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    driver = webdriver.Chrome('C:\\Users\\CKIRUser\\Downloads\\chromedriver_win32\\chromedriver.exe',options=options)
+    options.add_argument('window-size=1920x1080')
+    driver = webdriver.Chrome('C:\\Users\\CKIRUser\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe', options = options)
+
+    list = []
+
 
     def Blog_Naver():
         try:
+            global list
             Url = 'https://blog.naver.com/bjdmc4/222616996072'
             mail = 0
-            driver.get(url = Url)
+            driver.get(url=Url)
+
+
             driver.switch_to.frame('mainFrame')
-            driver.find_element(By.XPATH, '//*[@id="Comi222616996072"]').click()
+            com = driver.find_element(By.XPATH, '//*[@id="Comi222616996072"]')
+            sleep(2)
+            com.click()
             count = 0
             while count < 2:
+                list = []
                 html = driver.page_source
                 soup = BeautifulSoup(html, 'html.parser')
                 comments = soup.find_all('div', {'class': 'u_cbox_text_wrap'})
 
-                comments = extract_email(comments)
-                check_mail(mail, comments)
-                if (Check_Last_Mail == True):
+                for comment in comments:
+                    list.append(comment.get_text().strip())
+                list = extract_email(list)
+                Check = check_mail(mail, list)
+
+                if (Check == True):
                     break
                 else:
-                    btn_more = driver.find_element(By.XPATH, '//*[@id="naverComment_201_222616996072_ct"]/div[1]/div/div[2]/a[1]')
-                    btn_more.click()
+                    driver.implicitly_wait(5)
+                    left = driver.find_element(By.XPATH, '//*[@id="naverComment_201_222616996072_ct"]/div[1]/div/div[2]/a[1]')
+                    left.click()
                     prev = soup.find('strong', {'class': '_currentPageNo'})
                     prev_num = int(prev.get_text().strip())
-
-                    if (prev_num == 1):
-                        count += 1
-
+                    print(prev_num)
+                if (prev_num == 1):
+                    count += 1
             driver.quit()
         except:
-            print("Error, Please modify the code!!")
+            print('Error!! Modify the code please...\n')
+
+    root = Tk()
+    root.title("Practice")
+    # 버튼을 여러번 눌러서 항상 새로운 메일이 들어올 수 있도록 준비를 해놓는 것이 어떤지...
+    # 그리고 지금은 끝에 있는 메일을 받아오지만 조금 더 향상시키기 위해서는 Mail들을 리스트로 받아온 다음에 맨 끝 메일을 저장해두는 것이 어떤가 싶다. ( 프로그램이 켜져있는 동안에만 )
+    btn = Button(root, text='Please..', command=Blog_Naver)
+    btn.pack()
 
 
+    root.mainloop()
