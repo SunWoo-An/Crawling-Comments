9c92ed043f4da69ac401e736c8024bbf29d25247
SunWoo-An <84854953+SunWoo-An@users.noreply.github.com>
2022-02-03 23:52:57 +0900

    Update Functions.py

--- GUI_INNER_FUNCTION/Functions.py
+++ GUI_INNER_FUNCTION/Functions.py
@@ -1,150 +1,130 @@
+import re
+import os
 from tkinter import *
-from tkinter.messagebox import *
+import pyperclip
+from time import sleep
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+check_mail = False
+
+def Blog_Naver(Url, mail, ID, Pwd):  # 1번째 인자 : 맨 끝에 있는 메일주소 , 2번째 인자 : ID , 3번째 인자 : Password
+    Defined_list = []
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome('C:\\Users\\CKIRUser\\Downloads\\chromedriver_83version.exe', options=options)
+    global check_mail
+    global is_error
+    is_error = False
+    # try:
+    driver.get(url=Url)
+    driver.switch_to.frame('mainFrame')
+
+    # 블로그 안에서 바로 로그인 버튼을 누름.
+    driver.find_element(By.XPATH, '//*[@id="gnb-area"]/ul/li[4]/a').click()
+    # ID pwd 카피한 후 자동입력
+    pyperclip.copy(ID)
+    driver.find_element(By.ID, 'id').send_keys(Keys.CONTROL + 'v')
+    sleep(1)
+    pyperclip.copy(Pwd)
+    driver.find_element(By.ID, 'pw').send_keys(Keys.CONTROL + 'v')
+    driver.implicitly_wait(3)
+    driver.find_element(By.XPATH, '//*[@id="log.login"]').click()
+    driver.implicitly_wait(3)
+
+    driver.switch_to.frame('mainFrame')
+    com = driver.find_element(By.XPATH, '//*[@id="Comi222616996072"]')
+
+    sleep(1)
+    com.click()
+    count = 0
+    while count < 2:
+        lists = []
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        comments = soup.find_all('div', {'class': 'u_cbox_text_wrap'})
+        for comment in comments:
+            lists.append(comment.get_text().strip())
+        lists = extract_email(lists)
+        Defined_list.extend(check_mail(mail, lists))
+        if (check_mail == True):
+            break
+        else:
+            driver.implicitly_wait(5)
+            left = driver.find_element(By.XPATH,
+                                       '//*[@id="naverComment_201_222616996072_ct"]/div[1]/div/div[2]/a[1]')
+            left.click()
+            prev = soup.find('strong', {'class': '_currentPageNo'})
+            prev_num = int(prev.get_text().strip())
+            print(prev_num)
+        if (prev_num == 1):
+            count += 1
+    insert_txt(Defined_list)
+    driver.quit()
+
+    Defined_list = list(map(lambda x:x+',',Defined_list))
+    return Defined_list
+
+    # except:
+    # is_error = True
+    # return False
 
 
-class GUI_show():
-    # 초깃값
-    global blog_addres, ids, pwd
-    blog_address, ids, pwd = False, False, False
-
-
-    def __init__(self,email_lists):
-        self.email_lists = email_lists
-
-
-    def show(self):
-        global blog_address, ids, pwd
-        root = Tk()
-        root.resizable(False, False)
-        '''email_string = 'eunj0407@naver.com,sunkung0118@naver.com,sion011218@naver.com,sumsum3_@naver.com,hhyowon020304@gmail.com,eh040117@gmail.com,only4352@naver.com,thdnjs9853@naver.com,cmcm0722@gmail.com,softy05@naver.com,choco119600@gmail.com,sonchaeyeon1234@naver.com,v_mins2@naver.com,ohdayoung01@naver.com,seojin0441@naver.com,yeonsue05@naver.com,ccuuss04@gmail.com,wave04300@naver.com,emdeo203917@gmail.com,irin5501@gmail.com,emart8282@naver.com,lencoa24@naver.com,77greenb@naver.com,mjh2600@naver.com,dldlfdyd1119@naver.com,1028msj@naver.com,aa01037999435@gmail.com,jsoyoung05@gmail.com,ebmhk@naver.com,zziczzic94@gmail.com,ham2992@naver.com,soyeon4e9@naver.com,singsing1209@naver.com,yejunchae9156@gmail.com,ukfydu6850@naver.com,wintersnow13579@naver.com,yewon050705@naver.com,cats1030905@gmail.com,psy050622@naver.com,pinkyena1565@naver.com,aureve2587@naver.com,happykuna@naver.com,sungil0238@gmail.com,forkhm05@naver.com,dongdong9830@gmail.com,erikarina8075@gmail.com,yjyun011@naver.com,smj050228@naver.com,loveyys96@naver.com,vhffns@naver.com,ywseo0510@gmail.com,jby0324@naver.com,lunajo1009@naver.com,dayeon932@naver.com,yerin060301@naver.com,chngh1507@naver.com,eriel7754@naver.com,ijisu020@naver.com,sarangbaek0315@naver.com,eungyo9159@hanmail.net,okuk1226@naver.com,hyebin0704@naver.com,alsrud4359@naver.com,s1930332@naver.com,sp382150@dwfl.hs,saebomyoon07@naver.com,gmldnjsdl1223@naver.com,khb060306@naver.com,frontks0509@naver.com,svtcarat0814@naver.com,a7397425@gmail.com,gayeonii0930@naver.com,kimmijin6911@gmail.com,skaco544@gmail.com,raini0221@naver.com,ldr4671@naver.com,crom3946@gmail.com,kimyoubin4953@gmail.com,lucy5305@naver.com,jcan2424@gmail.com,kim0928naye@icloud.com,sonsy1209@icloud.com,kyaamy@naver.com,dogyung234@naver.com,wjdtmddbs1023@naver.com,haseon0102@gmail.com,byeonmingdo@naver.com,ksh060705@naver.com,koomj06200@naver.com,hjo04@naver.com,kaylucyer@naver.com,hyungyu_lee@naver.com,junnysweb@naver.com,minsung05k@gmail.com,gwen061004@naver.com,wjswl716@naver.com,yejin-9100@naver.com,eunsungpari@gmail.com,loveluv1312@naver.com,chaeming1026@gmail.com,mintsh333@naver.com,lucy08270@naver.com,jiminkyung38@gmail.com,peacholivia@naver.com,eunchaeh2014@naver.com,free01222@naver.com,seohee0227@naver.com,lilly3987@naver.com,mynation3@naver.com,choihwanse71@naver.com,dlcodus521@naver.com,kmckmc1304@naver.com,whitejoomi@naver.com,dlys_019@naver.com,buup13@naver.com,tkdhs0821@naver.com,parkny570450@gmail.com,a34607211@gmail.com,luna2491@naver.com,mj060907@naver.com,tlswltn0524@naver.com,10dbswls@naver.com,heysaysuji@naver.com,jhplus07@naver.com,a01054529663@gmail.com,agavond04@gmail.com,angeldudu@hanmail.net,hyes0000@naver.com,qhruddl0702@naver.com,hyojin1239@naver.com,tony5240@naver.com,seoyoung119@naver.com,hyunwoo2113@naver.com,gwon3288@naver.com,olivia625@naver.com,eunseo4215@naver.com,gksrkdus03@naver.com,wnehdgus0426@naver.com,yoon_2008@naver.com,2005dltkddnjs@naver.com,rigyeong6292@naver.com,sy_3117@naver.com,07eunseo0302@naver.com,ukiteim@gmail.com,leeyubin0413@gmail.com,1004_suyeon@naver.com,hpply5420@naver.com,as050419@naver.com,jisu6712@naver.com,i_believe33@naver.com,gmltmd123444@gmail.com'
-        
-        email_lists = email_string.split(',')
-        
-        '''
-        printed = ''
-
-        # 컴포넌트 정의
-        root.title('댓글 email 가져오기')
-
-        blog_address_label = Label(root, text='네이버 블로그 주소')
-        blog_address_entry = Entry(root)
-
-        id_label = Label(root, text='ID')
-        id_entry = Entry(root)
-
-        pwd_label = Label(root, text='PWD')
-        pwd_entry = Entry(root)
-
-        ok_button_1 = Button(root, text='확인')
-
-        null_label1 = Label(root, text='''
-         ''')
-
-
-        null_label2 = Label(root, text='''
-            ''')
-
-        from_email_label = Label(root, text='어떤 email부터 가져올까요?')
-        from_email_entry = Entry(root)
-        ok_button_2 = Button(root, text='확인')
-
-
-
-        def ok_button_1_click(string):
-            global crawling_cmt_label, blog_address, ids, pwd
-            blog_address = blog_address_entry.get()
-            ids = id_entry.get()
-            pwd = pwd_entry.get()
-
-            if blog_address and ids and pwd and (True):
-                crawling_cmt_label = Label(root, text='해당 포스트의 댓글에 달린 이메일들을 가져왔습니다!')
-                crawling_cmt_label.grid(row=4, column=0, columnspan=3, sticky='ew')
+def check_mail(mail, comments):
+    i = 0
+    global check_mail
+    last_list = []
+    if (str(mail) == '0'):
+        for comment in comments:
+            last_list.append(comment)
+    else:
+        # for문이 아닌 자료구조를 통해서 쉽게 search를 먼저 한 뒤에 list 에 넣는 방법을 생각해보자.
+        for comment in comments:
+            if (comment != mail):
+                last_list.append(comment)
             else:
-                showerror("Error", "잘못된 정보가 있습니다.\n다시 확인해주세요.")
-                return 0
-
-            null_label2.grid(row=5, column=0, columnspan=3, sticky='ew')
-
-            from_email_label.grid(row=6, column=0, sticky='ew')
-            from_email_entry.grid(row=6, column=1, sticky='ew')
-
-            ok_button_2.grid(row=6, column=2, sticky='ew')
+                check_mail = True
+                break
+    return last_list
 
 
-
-        print(ids)
-
-        ok_button_1.bind('<Button-1>', ok_button_1_click)
-
-
-        def ok_button_2_click(string):
-
-            global blog_address, ids, pwd
-            print(ids)
-
-            if blog_address and ids and pwd:
-                std_email = from_email_entry.get()
-                # std_email = zziczzic94@gmail.com
-
-                while True:
-                    if std_email in self.email_lists:
-
-                        idx = self.email_lists.index(std_email)
-                        result_list = self.email_lists[:idx + 1]
-                        result_list = list(map(lambda x: x + ',', result_list))
-
-                        showinfo('실행 완료', '이메일을 가져왔습니다. "결과 출력" 버튼을 눌러주세요.')
-                        print_clear_label = Label(root, text='이메일을 모두 가져왔습니다!')
-                        print_clear_label.grid(row=7, column=0, sticky='ew')
-                        with open('result.txt', 'w') as f:
-                            for i in result_list:
-                                f.write(i)
-
-                        view_file_button = Button(root, text='결과 출력')
-                        view_file_button.grid(row=7, column=1, columnspan=2, sticky='ew')
-
-                        def view_file_button_click(string):
-                            with open('result.txt', 'r') as f:
-                                printed = f.read()
-
-                            frame = Frame(root)
-                            scrollbar = Scrollbar(frame)
-                            scrollbar.pack(side='right', fill='y')
-                            result_text = Text(frame, yscrollcommand=scrollbar.set)
-                            result_text.insert(1.0, printed)
-                            result_text.configure(state='disabled')
-                            result_text.pack(fill="both", expand=True)
-                            scrollbar.config(command=result_text.yview)
-                            frame.grid(row=8, column=0, columnspan=3, sticky='sn')
-
-                        view_file_button.bind('<Button-1>', view_file_button_click)
-                        break
-
-                    else:
-                        showerror("Error", "해당 이메일은 댓글에 없습니다.\n 다시 입력해주세요.")
-                        return 0
-
-            else:
-                showerror('Error', '먼저 블로그 주소와 아이디, 패스워드를 입력해 주세요.')
-                return 0
-
-            view_file_button.bind('<Button-1>', view_file_button_click)
+def insert_txt(list_name):
+    with open('Defined Lists.txt', 'w', encoding='UTF-8') as f:
+        for line in list_name:
+            f.writelines(line)
 
 
-        ok_button_2.bind('<Button-1>', ok_button_2_click)
+def email_preprocessing(string):
+    valid_email = re.compile('[A-Za-z0-9-_+]+@[a-z]+[.]+[a-z]+')
+    result = valid_email.search(string)
 
-        # 배치
-        blog_address_label.grid(row=0, column=0, sticky='ew')
-        blog_address_entry.grid(row=0, column=1, sticky='ew')
+    if result:
+        return result.group()
 
-        id_label.grid(row=1, column=0, sticky='ew')
-        id_entry.grid(row=1, column=1, sticky='ew')
+    else:
+        return result
 
-        pwd_label.grid(row=2, column=0, sticky='ew')
-        pwd_entry.grid(row=2, column=1, sticky='ew')
 
-        ok_button_1.grid(row=0, column=3, rowspan=3, sticky='sn')
+def extract_email(comment):
+    result = []
 
-        null_label1.grid(row=3, column=0, columnspan=3, sticky='ew')
+    for each_comment in comment:
+        comment_token = each_comment.split(' ')
+        if email_preprocessing(comment_token[0]):
+            preclean = email_preprocessing(comment_token[0])
+            result.append(preclean)
 
-        root.mainloop()
+        elif email_preprocessing(comment_token[-1]):
+            preclean = email_preprocessing(comment_token[-1])
+            result.append(preclean)
+
+        else:
+            for tokens in comment_token:
+                if email_preprocessing(tokens):
+                    preclean = email_preprocessing(tokens)
+                    result.append(preclean)
+                    break
+
+    return result
