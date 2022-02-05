7533c3097910f8e0efc801260a7f892b3539fb4d
GitSion <74344298+GitSion@users.noreply.github.com>
2022-02-05 22:46:41 +0900

    Update GUIs.py

--- GUI_INNER_FUNCTION/GUIs.py
+++ GUI_INNER_FUNCTION/GUIs.py
@@ -1,11 +1,10 @@
 from tkinter import *
-from tkinter import filedialog
 from tkinter.messagebox import *
-import functions
-global blog_addres, ids, pwd
-from selenium import webdriver
+import main
 
+global blog_addres, ids, pwd
 blog_address, ids, pwd, from_email = False, False, False, False
+
 
 root = Tk()
 root.resizable(False, False)
@@ -14,8 +13,6 @@ printed = ''
 # 컴포넌트 정의
 root.title('댓글 email 가져오기')
 
-root.filename = filedialog.askopenfilename(initialdir="")
-
 blog_address_label = Label(root, text='네이버 블로그 주소')
 blog_address_entry = Entry(root)
 
@@ -23,9 +20,12 @@ id_label = Label(root, text='ID')
 id_entry = Entry(root)
 
 pwd_label = Label(root, text='PWD')
-pwd_entry = Entry(root, show = "*")
+pwd_entry = Entry(root, show="*")
 
-null_label1 = Label(root, text='-'*65)
+chrome_driver_place_label = Label(root, text='Chromedriver 위치')
+chrome_driver_place_entry = Entry(root)
+
+null_label1 = Label(root, text='-' * 65)
 
 from_email_label = Label(root, text='어떤 email부터 가져올까요?')
 from_email_entry = Entry(root)
@@ -39,15 +39,16 @@ def ok_button_click(string):
     blog_address = blog_address_entry.get()
     ids = id_entry.get()
     pwd = pwd_entry.get()
+    chrome_driver_place = chrome_driver_place_entry.get()
     from_email = from_email_entry.get()
 
     if blog_address and ids and pwd and from_email:
-        results = functions.Blog_Naver(blog_address,from_email,ids,pwd)
+        results = main.Blog_Naver(blog_address, from_email, ids, pwd, chrome_driver_place)
         if results:
             with open('result.txt', 'w') as f:
                 for i in results:
                     f.write(i)
-            showinfo('확인','원하시는 이메일들을 성공적으로 가져왔습니다.')
+            showinfo('확인', '원하시는 이메일들을 성공적으로 가져왔습니다.')
 
             with open('result.txt', 'r') as f:
                 printed = f.read()
@@ -64,16 +65,17 @@ def ok_button_click(string):
 
 
         else:
-            showerror('Error','입력된 값 중 잘못된 값이 있습니다.\n다시 확인해주세요.')
+            showerror('Error', '입력된 값 중 잘못된 값이 있습니다.\n다시 확인해주세요.')
             return 0
 
     else:
-        showerror('Error','입력 창에 빈칸이 있습니다.\n입력창을 모두 채워주세요.')
+        showerror('Error', '입력 창에 빈칸이 있습니다.\n입력창을 모두 채워주세요.')
         return 0
 
 
+root.bind('<Return>', ok_button_click)
 
-ok_button.bind('<Button-1>',ok_button_click)
+ok_button.bind('<Button-1>', ok_button_click)
 
 # 배치
 blog_address_label.grid(row=0, column=0, sticky='ew')
@@ -85,12 +87,14 @@ id_entry.grid(row=1, column=1, sticky='ew')
 pwd_label.grid(row=2, column=0, sticky='ew')
 pwd_entry.grid(row=2, column=1, sticky='ew')
 
+chrome_driver_place_label.grid(row=3, column=0, sticky='ew')
+chrome_driver_place_entry.grid(row=3, column=1, sticky='ew')
 
-null_label1.grid(row=3, column=0, columnspan=3, sticky='ew')
-from_email_label.grid(row=4, column=0, sticky='ew')
-from_email_entry.grid(row=4, column=1, sticky='ew')
+null_label1.grid(row=4, column=0, columnspan=3, sticky='ew')
+from_email_label.grid(row=5, column=0, sticky='ew')
+from_email_entry.grid(row=5, column=1, sticky='ew')
 
 ok_button.grid(row=0, column=2, rowspan=5, sticky='sn')
-null_label2.grid(row=5, column=0, columnspan=3, sticky='ew')
+null_label2.grid(row=6, column=0, columnspan=3, sticky='ew')
 
 root.mainloop()
